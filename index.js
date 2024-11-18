a832f621a6a7ba20d627671a6fa3699966fef655
EngineerVirajPatil <engineer.virajpatil@gmail.com>
2024-11-18 13:44:42 +0530

    Push 1

--- index.js
+++ index.js
@@ -2,13 +2,55 @@ const express = require('express');
 const { resolve } = require('path');
 
 const app = express();
-const port = 3010;
+const port = 3000;
 
-app.use(express.static('static'));
+let ages=[25,30,18,22,27];
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+let students=[{
+  name:"Rahul",
+  rollNo:101,
+  marks:85
+},
+{
+  name:"Sita",
+  rollNo:102,
+  marks:95
+},
+{
+  name:"Amit",
+  rollNo:103,
+  marks:70
+}];
+
+function sortAscendingOrder(age1,age2){
+ return age1-age2;
+}
+
+function sortDescendingOrder(age1,age2){
+  return age2-age1;
+ }
+
+ function sortStudentMarksInDescending(student1,student2){
+ return student2.marks-student1.marks;
+ }
+
+app.get('/ages/sort-ascending',(req, res)=>{
+  let agesCopy=ages.slice(); //slice() method is used to create copy of any array
+  agesCopy.sort(sortAscendingOrder);
+  res.json(agesCopy);
 });
+
+app.get('/ages/sort-descending',(req, res)=>{
+  let agesCopy=ages.slice(); //slice() method is used to create copy of any array
+  agesCopy.sort(sortDescendingOrder);
+  res.json(agesCopy);
+});
+
+app.get('/students/sort-by-marks-descending',(req, res)=>{
+ studentsCopy=students.slice();
+ studentsCopy.sort(sortStudentMarksInDescending);
+ res.json(studentsCopy);
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
